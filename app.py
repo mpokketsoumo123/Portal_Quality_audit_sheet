b9cd77b647b51d36d5d60bd4401915fe207602fd
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-18 16:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,12 @@ if uploaded_file is not None:
 """, unsafe_allow_html=True)
 
 # Display logo
-st.markdown('<img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/302bf6105854045.5f82a86549930.png" class="logo">', unsafe_allow_html=True)
+st.markdown("""
+    <div class="header">
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/302bf6105854045.5f82a86549930.png" class="logo"> 
+    </div>
+    """, unsafe_allow_html=True)
+#st.markdown('<img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/302bf6105854045.5f82a86549930.png" class="logo">', unsafe_allow_html=True)
 
 # Display bold header text
 #st.markdown('<div class="header-text">Onboarding Audit Portal</div>', unsafe_allow_html=True)

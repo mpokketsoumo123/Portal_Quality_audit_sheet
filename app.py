cdc808659cc5f00ac17724801db815cf51f7f891
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-19 14:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -632,16 +632,16 @@ elif selected_page == "Input Form":
                     updated_row[key] = st.text_input(f"{key}:", value=value)
 
     # Function to update the row
-    def update_row(row_index, updated_row):
-        st.session_state["input_table"][row_index] = updated_row
+        def update_row(row_index, updated_row):
+            st.session_state["input_table"][row_index] = updated_row
 
-    if st.button("Save Updated Row"):
-        update_row(adjusted_index, updated_row)
-        st.success("Row updated!")
+        if st.button("Save Updated Row"):
+            update_row(adjusted_index, updated_row)
+            st.success("Row updated!")
 
         # Show the updated dataframe
-        st.write("Updated Table:")
-        st.dataframe(pd.DataFrame(st.session_state["input_table"])) 
+            st.write("Updated Table:")
+            st.dataframe(pd.DataFrame(st.session_state["input_table"])) 
 
 
     # Final Submit Button

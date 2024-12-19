99cf76964bf9c63851be93f1c0673d3317b4a0b8
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-19 17:39:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ if uploaded_file is not None:
 # Display logo
 st.markdown("""
     <header>
-        <img src="302bf6105854045.5f82a86549930.png"> 
+        <img src="logo.png"> 
         <h1>Onboarding Audit Portal</h1>
     </header>
     """, unsafe_allow_html=True)

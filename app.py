787928ccd7b0c12ba6ec8dad97b8cb5ccb23a66e
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-20 13:22:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -604,26 +604,11 @@ elif selected_page == "Input Form":
         gb = GridOptionsBuilder.from_dataframe(df)
         
         # JavaScript code for the Update, Final Submit, and Delete buttons
-       update_button = JsCode("""
-            function(params) {
-                return `<button style="color:white; background-color:blue; padding:3px; border:none; border-radius:5px; cursor:pointer;" 
-                        onclick="handleUpdate(${params.rowIndex})">Update</button>`;
-                }
-            """)
+       update_button = JsCode("""function(params) {return `<button style="color:white; background-color:blue; padding:3px; border:none; border-radius:5px; cursor:pointer;" onclick="handleUpdate(${params.rowIndex})">Update</button>`; }""")
 
-        final_submit_button = JsCode("""
-            function(params) {
-                return `<button style="color:white; background-color:green; padding:3px; border:none; border-radius:5px; cursor:pointer;" 
-                        onclick="handleFinalSubmit(${params.rowIndex})">Final Submit</button>`;
-            }
-            """)
+        final_submit_button = JsCode("""function(params) {return `<button style="color:white; background-color:green; padding:3px; border:none; border-radius:5px; cursor:pointer;" onclick="handleFinalSubmit(${params.rowIndex})">Final Submit</button>`;}""")
         
-        delete_button = JsCode("""
-            function(params) {
-                return `<button style="color:white; background-color:red; padding:3px; border:none; border-radius:5px; cursor:pointer;" 
-                        onclick="handleDelete(${params.rowIndex})">Delete</button>`;
-                }
-            """)
+        delete_button = JsCode("""function(params) {return `<button style="color:white; background-color:red; padding:3px; border:none; border-radius:5px; cursor:pointer;" onclick="handleDelete(${params.rowIndex})">Delete</button>`;}""")
         # Add Action and Delete columns
         df["Action"] = [
             "Final Submit" if i == st.session_state["edit_row_index"] else "Update"

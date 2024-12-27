5cd61b279d04ee72655d7d37af3fafe9f8de7f9f
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-27 15:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ elif selected_page == "Input Form":
         calling_number = st.text_input("",key="calling_number",label_visibility="collapsed")
         #remarks
         st.markdown('<div class="custom-label">Remarks:</div>', unsafe_allow_html=True)
-        Remarks = st.text_area("",key="remarks",height=60)
+        Remarks = st.text_area("",key="remarks",height=68)
 
         
 

f95cdaa99150ec571e992643c086839f0852bc3e
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-27 16:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -811,7 +811,7 @@ elif selected_page == "Input Form":
         
             with col3:
                 st.markdown('<div class="custom-label">Select Operation:</div>', unsafe_allow_html=True)
-                operation = st.radio("", ["Update Row", "Delete Row"], key="operation",label_visibility="collapsed")
+                operation = st.radio("", ["Update Row", "Delete Row"], key="operation")
         
             with col4:  # Alignment adjustment
                 submit_button = st.form_submit_button("Submit")

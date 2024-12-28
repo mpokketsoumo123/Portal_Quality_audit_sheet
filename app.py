78194e3dadbc06f940c8fbadec9963d018b5749b
mpokketsoumo123 <soumodip.khan@mpokket.com>
2024-12-28 17:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ if selected_page == "Login":
         }
         .stTextInput {
         width: 300px; /* Set a fixed width for input boxes */
-        hight 100%;
+        hight:100%;
         margin-bottom: 15px;
         }
         
